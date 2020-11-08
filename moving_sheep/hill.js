16178d6a411587557338697681843fabda1a3418
taejin <taejin5314@gmail.com>
2020-11-07 23:03:50 -0500

    Create hill.js

---
+++ moving_sheep/hill.js
@@ -0,0 +1,22 @@
+export class Hill {
+    constructor(color, speed, total) {
+        this.color = color;
+        this.speed = speed;
+        this.total = total;
+    }
+
+    resize(stageWidth, stageHeight) {
+        this.stageWidth = stageWidth;
+        this.stageHeight = stageHeight;
+
+
+    }
+
+    draw(ctx) {
+
+    }
+
+    getY() {
+
+    }
+}
