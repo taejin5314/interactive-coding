d2e41d56d332f09ccf838c8eb8d36772926b9988
taejin <taejin5314@gmail.com>
2020-11-09 22:32:20 -0500

    Adding sun

--- moving_sheep/app.js
+++ moving_sheep/app.js
@@ -6,11 +6,17 @@ import {
     SheepController
 } from './sheep-controller.js';
 
+import {
+    Sun
+} from './sun.js';
+
 class App {
     constructor() {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
+
+        this.sun = new Sun();
 
         this.hills = [
             new Hill('#fd6bea', 0.2, 12),
@@ -34,6 +40,8 @@ class App {
         this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2, 2);
 
+        this.sun.resize(this.stageWidth, this.stageHeight);
+
         for (let i = 0; i < this.hills.length; i++) {
             this.hills[i].resize(this.stageWidth, this.stageHeight);
         }
@@ -45,6 +53,8 @@ class App {
         requestAnimationFrame(this.animate.bind(this));
 
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        this.sun.draw(this.ctx, t);
 
         let dots;
         for (let i = 0; i < this.hills.length; i++) {
