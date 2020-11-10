f779dc5d1d7679bc50dc1f3b5797fb719c088041
taejin <taejin5314@gmail.com>
2020-11-09 21:24:17 -0500

    Moving Hills

--- moving_sheep/hill.js
+++ moving_sheep/hill.js
@@ -28,15 +28,26 @@ export class Hill {
         let prev = cur;
 
         let dots = [];
+        cur.x += this.speed;
+
+
+        if (cur.x > -this.gap) {
+            this.points.unshift({
+                x: -(this.gap * 2),
+                y: this.getY()
+            });
+        } else if (cur.x > this.stageWidth + this.gap) {
+            this.points.splice(-1);
+        }
 
         ctx.moveTo(cur.x, cur.y);
 
         let prevCx = cur.x;
         let prevCy = cur.y;
 
-        for (let i = 0; i < this.points.length; i++) {
+        for (let i = 1; i < this.points.length; i++) {
             cur = this.points[i];
-
+            cur.x += this.speed;
             const cx = (prev.x + cur.x) / 2;
             const cy = (prev.y + cur.y) / 2;
             ctx.quadraticCurveTo(prev.x, prev.y, cx, cy);
