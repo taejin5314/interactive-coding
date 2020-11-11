b4743d40aea7229141d37b8055ce33c2988e3d8e
taejin <taejin5314@gmail.com>
2020-11-10 20:21:59 -0500

    Update app.js

---
+++ polygon/app.js
@@ -0,0 +1,34 @@
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.staegHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.staegHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+    }
+
+    animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.staegHeight);
+    }
+}
+
+window.onload = () => {
+    new App();
+}
