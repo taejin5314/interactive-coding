178933a7fa63f5dba68f5410299b5998afcca4b0
taejin <taejin5314@gmail.com>
2020-11-10 20:36:50 -0500

    Update polygon.js

--- polygon/app.js
+++ polygon/app.js
@@ -11,6 +11,10 @@ class App {
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
 
+        this.isDown = false;
+        this.moveX = 0;
+        this.offsetX = 0;
+
         document.addEventListener('pointerdown', this.onDown.bind(this), false);
         document.addEventListener('pointermove', this.onMove.bind(this), false);
         document.addEventListener('pointerup', this.onUp.bind(this), false);
@@ -30,7 +34,7 @@ class App {
             this.stageWidth / 2,
             this.staegHeight / 2,
             this.staegHeight / 3,
-            5
+            10
         );
     }
 
@@ -39,8 +43,28 @@ class App {
 
         this.ctx.clearRect(0, 0, this.stageWidth, this.staegHeight);
 
-        this.polygon.animate(this.ctx);
+        this.moveX *= 0.92;
+
+        this.polygon.animate(this.ctx, this.moveX);
     }
+
+    onDown(e) {
+        this.isDown = true;
+        this.moveX = 0;
+        this.offsetX = e.clientX;
+    }
+
+    onMove(e) {
+        if (this.isDown) {
+            this.moveX = e.clientX - this.offsetX;
+            this.offsetX = e.clientX;
+        }
+    }
+
+    onUp(e) {
+        this.isDown = false;
+    }
+
 }
 
 window.onload = () => {
--- polygon/polygon.js
+++ polygon/polygon.js
@@ -9,24 +9,39 @@ export class Polygon {
         this.rotate = 0;
     }
 
-    animate(ctx) {
+    animate(ctx, moveX) {
         ctx.save();
         ctx.fillStyle = '#000';
-        ctx.beginPath();
+        // ctx.beginPath();
 
         const angle = PI2 / this.sides;
+        const angle2 = PI2 / 4;
 
         ctx.translate(this.x, this.y);
+
+        this.rotate -= moveX * 0.008;
+        ctx.rotate(this.rotate);
 
         for (let i = 0; i < this.sides; i++) {
             const x = this.radius * Math.cos(angle * i);
             const y = this.radius * Math.sin(angle * i);
 
-            (i == 0) ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
+            ctx.save();
+            ctx.translate(x, y);
+            ctx.rotate(((360 / this.sides) * i + 45) * Math.PI / 180);
+            ctx.beginPath();
+            for (let j = 0; j < 4; j++) {
+                const x2 = 80 * Math.cos(angle2 * j);
+                const y2 = 80 * Math.sin(angle2 * j);
+                (j == 0) ? ctx.moveTo(x2, y2) : ctx.lineTo(x2, y2);
+            }
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore();
         }
 
-        ctx.fill();
-        ctx.closePath();
+        // ctx.fill();
+        // ctx.closePath();
         ctx.restore();
     }
 }
