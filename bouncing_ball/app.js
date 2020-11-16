b985cfa4ced35619b55c406880b9521e284883f2
taejin <taejin5314@gmail.com>
2020-11-15 20:49:20 -0500

    Update block.js

--- bouncing_ball/app.js
+++ bouncing_ball/app.js
@@ -1,4 +1,5 @@
 import { Ball } from './ball.js';
+import { Block } from './block.js';
 
 class App {
     constructor() {
@@ -11,6 +12,7 @@ class App {
         this.resize();
 
         this.ball = new Ball(this.stageWidth, this.stageHeight, 60, 15);
+        this.block = new Block(700, 30, 250, 200);
 
         window.requestAnimationFrame(this.animate.bind(this));
     }
@@ -26,6 +28,10 @@ class App {
 
     animate(t) {
         window.requestAnimationFrame(this.animate.bind(this))
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        this.block.draw(this.ctx)
 
         this.ball.draw(this.ctx, this.stageWidth, this.stageHeight);
     }
