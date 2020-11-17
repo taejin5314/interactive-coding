a3e70c55869004a1c5c3b64ca906aec562b63f1f
taejin <taejin5314@gmail.com>
2020-11-16 21:05:37 -0500

    Update ripple.js

--- ripple_color_dots/ripple.js
+++ ripple_color_dots/ripple.js
@@ -6,4 +6,24 @@ export class Ripple {
         this.maxRadius = 0;
         this.speed = 10;
     }
+
+    resize(stageWidth, stageHeight) {
+        this.stageWidth = stageWidth;
+        this.stageHeight = stageHeight;
+    }
+
+    start(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = 0;
+        this.maxRadius = this.getMax(x, y);
+    }
+
+    animate() {
+
+    }
+
+    getMax(x, y) {
+
+    }
 }
