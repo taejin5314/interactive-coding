89b3212c5432da87461c95b69f682c154e870ba1
taejin <taejin5314@gmail.com>
2020-11-18 19:20:56 -0500

    Update app.js

--- kinetic_typography/app.js
+++ kinetic_typography/app.js
@@ -1,21 +1,38 @@
-import { Text } from './text.js';
-
 class App {
     constructor() {
+        this.setWebgl();
+
         WebFont.load({
             google: {
                 families: ['Hind:700']
             },
             fontactive: () => {
-                this.text = new Text();
-                this.text.setText(
-                    'A',
-                    2,
-                    document.body.clientWidth,
-                    document.body.clientHeight,
-                )
+                window.addEventListener('resize', this.resize.bind(this), false);
+                this.resize();
+
+                requestAnimationFrame(this.animate.bind(this));
             }
         });
+    }
+
+    setWebgl() {
+        this.renderer = new PIXI.Renderer({
+            width: document.body.clientWidth,
+            height: document.body.clientHeight,
+            antialias: true,
+            transparent: false,
+            resolution: (window.devicePixelRatio > 1) ? 2 : 1,
+            autoDenstiy: true,
+            powerPreference: "high-performance",
+            backgroundColor: 0xffffff,
+        });
+        document.body.appendChild(this.renderer.view)
+
+        this.stage = new PIXI.Container();
+    }
+
+    resize() {
+
     }
 }
 
