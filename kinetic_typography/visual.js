58a785813cbdbd4bba8a9720416e821e37421b45
taejin <taejin5314@gmail.com>
2020-11-19 18:25:33 -0500

    Update visual.js

--- kinetic_typography/visual.js
+++ kinetic_typography/visual.js
@@ -1,3 +1,25 @@
 import { Text } from './text.js';
 import { Particle } from './particle.js';
 
+export class Visual {
+    constructor() {
+        this.text = new Text();
+
+        this.texture = PIXI.Texture.from('particle.png');
+
+        this.particles = [];
+
+        this.mouse = {
+            x: 0,
+            y: 0,
+            radius: 100
+        };
+
+        document.addEventListener('pointermove', this.onMove.bind(this), false);
+    }
+
+    onMove(e) {
+        this.mouse.x = e.clientX;
+        this.mouse.y = e.clientY;
+    }
+}
