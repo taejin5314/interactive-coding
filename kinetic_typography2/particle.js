83364729908b1744bf6995dc6de0ede9c2793172
taejin <taejin5314@gmail.com>
2020-11-23 17:28:18 -0500

    Update particle.js

--- kinetic_typography2/particle.js
+++ kinetic_typography2/particle.js
@@ -1,2 +1,27 @@
 const TOTAL = 12;
 
+export class Particle {
+    constructor(pos, color, ctx) {
+        this.color = color;
+
+        const ranMax = 20;
+        this.points = [{
+            x: pos.x,
+            y: pos.y,
+        }];
+
+        for (let i = 0; i < TOTAL; i++) {
+            const prev = this.points[i - 1];
+            this.points.push(this.setTandom(prev, ranMax));
+        }
+
+        this.draw(ctx);
+    }
+
+    setTandom(pos, gap) {
+        return {
+            x: pos.x + Math.random() * (gap + gap) - gap,
+            y: pos.y + Math.random() * (gap + gap) - gap,
+        }
+    }
+}
