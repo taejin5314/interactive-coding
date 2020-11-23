3ea87a47bd045418b54c32599e80c9a395e3ea7b
taejin <taejin5314@gmail.com>
2020-11-23 17:30:35 -0500

    Update particle.js

--- kinetic_typography2/particle.js
+++ kinetic_typography2/particle.js
@@ -18,6 +18,23 @@ export class Particle {
         this.draw(ctx);
     }
 
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.lineWidth = 0.3;
+        ctx.strokeStyle = this.color;
+        ctx.moveTo(this.points[0].x, this.points[0].y);
+
+        for (let i = 1; i < this.points.length; i++) {
+            const prev = this.points[i - 1];
+            const cur = this.points[i];
+            const cx = (prev.x + cur.x) / 2;
+            const cy = (prev.y + cur.y) / 2;
+            ctx.quadraticCurveTo(prev.x, prev.y, cx, cy);
+        }
+
+        ctx.stroke();
+    }
+
     setTandom(pos, gap) {
         return {
             x: pos.x + Math.random() * (gap + gap) - gap,
