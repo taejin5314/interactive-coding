3a95fabc1dc3e347a029a0f37b2f92b9f082b058
taejin <taejin5314@gmail.com>
2020-11-23 18:05:15 -0500

    udpate

--- kinetic_typography2/app.js
+++ kinetic_typography2/app.js
@@ -1,19 +1,38 @@
+import { Visual } from './visual.js';
 import { Text } from './text.js';
+import { setColor } from './color.js';
 
 class App {
     constructor() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        this.thumbs = [];
+
         WebFont.load({
             google: {
                 families: ['Hind:700']
             },
             fontactive: () => {
+                const ul = document.getElementsByTagName('ul')[0];
+                const lis = ul.getElementsByTagName('li');
+                for (let i = 0; i < lis.length; i++) {
+                    const item = lis[i];
+                    const img = item.getElementsByTagName('img')[0];
+                    item.addEventListener('click', e => {
+                        this.show(i);
+                    }, false);
+
+                    this.thumbs[i] = {
+                        item,
+                        img: img.src,
+                    };
+                }
+
                 this.text = new Text();
-                this.text.setText(
-                    'A',
-                    2,
-                    document.body.clientWidth,
-                    document.body.clientHeight,
-                )
             }
         });
     }
--- kinetic_typography2/color.js
+++ kinetic_typography2/color.js
@@ -4,7 +4,21 @@ export async function setColor(url) {
         image.src = url;
         image.onload = () => {
             const tmpCanvas = document.createElement('canvas');
+            tmpCanvas.width = image.width;
+            tmpCanvas.height = image.height;
+            const tmpCtx = tmpCanvas.getContext('2d');
 
+            tmpCtx.drawImage(
+                image,
+                0, 0,
+                image.width, image.height
+            );
+
+            resolve({
+                colorCtx: tmpCtx,
+                widht: image.width,
+                height: image.height
+            })
         }
     })
 }
