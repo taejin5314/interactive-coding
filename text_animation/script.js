07637ea2796f4cadf56a101bc9b98e35376e7282
taejin <taejin5314@gmail.com>
2020-11-29 14:57:24 -0500

    Update script.js

--- text_animation/script.js
+++ text_animation/script.js
@@ -34,17 +34,28 @@ class Particle {
         this.density = (Math.random() * 30) + 1;
     }
     draw() {
-        ctx.fillStyle = 'white';
+        ctx.fillStyle = 'red';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.closePath();
         ctx.fill();
     }
+
+    update() {
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < 300) {
+            this.size = 20;
+        } else {
+            this.size = 3;
+        }
+    }
 }
 
 function init() {
     particleArray = [];
-    for (let i = 0; i < 500; i++) {
+    for (let i = 0; i < 1000; i++) {
         let x = Math.random() * canvas.width;
         let y = Math.random() * canvas.height;
         particleArray.push(new Particle(x, y));
@@ -60,6 +71,7 @@ function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < particleArray.length; i++) {
         particleArray[i].draw();
+        particleArray[i].update();
     }
     requestAnimationFrame(animate);
 }
