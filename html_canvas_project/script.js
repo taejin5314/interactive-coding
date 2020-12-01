5f966c2f005cd1bc0b010de172add00893bbafca
taejin <taejin5314@gmail.com>
2020-12-01 17:07:30 -0500

    Update script.js

--- html_canvas_project/script.js
+++ html_canvas_project/script.js
@@ -1,3 +1,48 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-console.log(ctx);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particlesArray = [];
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = 10;
+        this.weight = 2;
+        this.directionX = -2;
+    }
+
+    update() {
+        if (this.y > canvas.height) {
+            this.y = 0 - this.size;
+            this.weight = 2;
+            this.x = Math.random() * canvas.width;
+            this.y = Math.random() * canvas.height;
+        }
+        this.weight += 0.15;
+        this.y += this.weight;
+        this.x += this.directionX;
+    }
+
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+const particle1 = new Particle(100, 50);
+
+function animate() {
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.01)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    particle1.update();
+    particle1.draw();
+
+    requestAnimationFrame(animate);
+}
+
+animate();
