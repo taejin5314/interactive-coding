d09d8d600dd452fec74f60e669289e1f314d80b9
taejin <taejin5314@gmail.com>
2020-12-03 17:05:05 -0500

    drawing circle

---
+++ drawing_flower/script.js
@@ -0,0 +1,25 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let size = 0;
+let positionX = 0;
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    size += 0.05;
+    positionX += 0.5
+
+    ctx.fillStyle = 'blue';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.arc(positionX, 300, size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+
+    requestAnimationFrame(animate);
+}
+animate();
