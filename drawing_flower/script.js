f7972529e0d25aa32a83120ae472116eb1c17ce2
taejin <taejin5314@gmail.com>
2020-12-03 17:11:37 -0500

    moving circular

--- drawing_flower/script.js
+++ drawing_flower/script.js
@@ -3,22 +3,30 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let size = 0;
-let positionX = 0;
+let positionX = canvas.width / 2;
+let positionY = canvas.height / 2;
+let angle = 0;
 
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    size += 0.05;
-    positionX += 0.5
+function drawFlower() {
+    // clearing the canvas each time the function is called
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     ctx.fillStyle = 'blue';
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 5;
     ctx.beginPath();
-    ctx.arc(positionX, 300, size, 0, Math.PI * 2);
+    ctx.arc(positionX, positionY, 20, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
+}
+
+function animate() {
+    positionX += 5 * Math.sin(angle);
+    positionY += 5 * Math.cos(angle);
+    angle += 0.1;
+
+    drawFlower();
 
     requestAnimationFrame(animate);
 }
