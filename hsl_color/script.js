c5d2646c3b4a59bd5439736bf9bcf44bcf975d17
taejin <taejin5314@gmail.com>
2020-12-02 22:07:40 -0500

    Update script.js

--- hsl_color/script.js
+++ hsl_color/script.js
@@ -4,10 +4,52 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let numberOfParticles = 200;
 const particleArray = [];
-let hue = 0;
+let hue = Math.random() * 500;
 
 class Particle {
     constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.radius = (Math.random() * 10) + 2;
+        this.speedX = (Math.random() * 3) - 1.5;
+        this.speedY = (Math.random() * 3) - 1.5;
+    }
 
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+        ctx.fill();
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.speedX = -this.speedX;
+        }
+        if (this.Y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.speedY = -this.speedY;
+        }
+        this.draw();
     }
 }
+
+function init() {
+    for (let i = 0; i < numberOfParticles; i++) {
+        particleArray.push(new Particle());
+    }
+}
+
+function animate() {
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < numberOfParticles; i++) {
+        particleArray[i].update();
+    }
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
