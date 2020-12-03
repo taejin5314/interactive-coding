90d4563328f056e28e2a0f7dca78097875df5af4
taejin <taejin5314@gmail.com>
2020-12-03 17:28:44 -0500

    Update script.js

--- drawing_flower/script.js
+++ drawing_flower/script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.globalCompositeOperation = 'destination-over';
 
 let number = 0;
 let scale = 10;
@@ -9,16 +10,16 @@ let scale = 10;
 function drawFlower() {
     // clearing the canvas each time the function is called
     // ctx.clearRect(0, 0, canvas.width, canvas.height)
-    let angle = number * 1;
+    let angle = number * 9;
     let radius = scale * Math.sqrt(number);
-    let positionX = radius * Math.sin(angle);
-    let positionY = radius * Math.cos(angle);
+    let positionX = radius * Math.sin(angle) + canvas.width / 2;
+    let positionY = radius * Math.cos(angle) + canvas.height / 2;
 
-    ctx.fillStyle = 'blue';
-    ctx.strokeStyle = 'black';
+    ctx.fillStyle = 'rose';
+    ctx.strokeStyle = 'white';
     ctx.lineWidth = 5;
     ctx.beginPath();
-    ctx.arc(positionX, positionY, 20, 0, Math.PI * 2);
+    ctx.arc(positionX, positionY, number, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
@@ -28,7 +29,7 @@ function drawFlower() {
 
 function animate() {
     drawFlower();
-
+    if (number > 200) return;
     requestAnimationFrame(animate);
 }
 animate();
