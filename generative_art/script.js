d09586e1341b0d8af6c945a54fba8f318d4fb482
taejin <taejin5314@gmail.com>
2020-12-03 18:17:43 -0500

    Update script.js

--- generative_art/script.js
+++ generative_art/script.js
@@ -15,3 +15,26 @@ window.addEventListener('mousemove', function (event) {
     mouse.y = event.y;
     console.log(mouse)
 })
+
+class Root {
+    constructor(x, y, color, centerX, centerY) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+
+    draw() {
+        this.speedX += (Math.random() - 0.5) * 2;
+        this.speedY += (Math.random() - 0.5) * 2;
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        const distanceX = this.x - this.centerX;
+        const distanceY = this.y - this.centerY;
+
+    }
+}
