4394d0a825c780249525c0cdeecba30e5bc4bfd5
taejin <taejin5314@gmail.com>
2020-12-04 18:10:25 -0500

    Update script.js

--- generative_art/script.js
+++ generative_art/script.js
@@ -65,6 +65,7 @@ window.addEventListener('resize', function () {
 });
 
 window.addEventListener('mousemove', function () {
+    ctx.fillStyle = 'rgba(0, 0, 255, 0.03)';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     branchOut();
 })
