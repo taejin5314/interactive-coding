50098d69b7045f1766738e829c4d6d3029129edf
taejin <taejin5314@gmail.com>
2020-12-07 17:06:51 -0500

    Update script.js

--- text_animation2/script.js
+++ text_animation2/script.js
@@ -1,3 +1,21 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = window.in
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.textBaseline = 'middle';
+let lettersArray = ['T', 'A', 'E', 'J', 'I', 'N'];
+let hue = 0;
+let particles = [];
+let numberOfParticles = (canvas.width * canvas.height) / 5000;
+
+const mouse = {
+    x: 0,
+    y: 0,
+    radius: 0,
+    autopilotAngle: 0,
+}
+
+window.addEventListener('mousemove', function (e) {
+    mouse.x = e.x;
+    mouse.y = e.y;
+})
