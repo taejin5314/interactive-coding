59b6ec5cd8d5cf4ca7718dbb7d86085329c83114
taejin <taejin5314@gmail.com>
2020-12-10 17:20:33 -0500

    Update script.js

--- bubble_text/script.js
+++ bubble_text/script.js
@@ -21,3 +21,24 @@ ctx.fillStyle = 'white';
 ctx.font = '25px Verdana';
 ctx.fillText('A', 0, 30);
 const textCoordinates = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = 3;
+        this.baseX = this.x;
+        this.baseY = this.y;
+        this.density = (Math.random() * 30) + 1;
+    }
+    draw() {
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+    }
+    update() {
+
+    }
+}
