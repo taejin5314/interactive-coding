784058f09b4176c98ed35bd52a6078252b963567
taejin <taejin5314@gmail.com>
2020-12-18 14:48:30 -0500

    Update script.js

---
+++ particle_animation/script.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const numberOfParticles = 200;
+let particlesArray = [];
+
+class Particle {
+    constructor() {
+
+    }
+}
