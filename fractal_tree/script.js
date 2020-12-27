80b47380b2f53fb64de4318f13706635b012a4cc
taejin <taejin5314@gmail.com>
2020-12-26 21:13:44 -0500

    Update script.js

---
+++ fractal_tree/script.js
@@ -0,0 +1,13 @@
+const CANVAS_WIDTH = 480;
+const CANVAS_HEIGHT = 620;
+
+let snowBgCanvas;
+let branchCanvas;
+
+function initializeCanvas(canvasID) {
+
+}
+
+function main() {
+    console.log('test');
+}
