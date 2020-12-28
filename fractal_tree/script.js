c04448c2cc7d81ad6df1857a43baa6128be2e5fe
taejin <taejin5314@gmail.com>
2020-12-27 20:11:53 -0500

    Update script.js

--- fractal_tree/script.js
+++ fractal_tree/script.js
@@ -5,9 +5,14 @@ let snowBgCanvas;
 let branchCanvas;
 
 function initializeCanvas(canvasID) {
-
+    const canvas = document.getElementById(canvasID);
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+    return canvas;
 }
 
 function main() {
     console.log('test');
+    snowBgCanvas = initializeCanvas('canvasSnowBackground');
+    branchCanvas = initializeCanvas('canvasTreeBranches')
 }
