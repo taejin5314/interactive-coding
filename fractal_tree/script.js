974f53709a0f2f161b7fb9124ad955442affd2ce
taejin <taejin5314@gmail.com>
2020-12-28 17:40:38 -0500

    Update script.js

--- fractal_tree/script.js
+++ fractal_tree/script.js
@@ -16,6 +16,36 @@ function main() {
     branchCanvas = initializeCanvas('canvasTreeBranches');
     const treeLocation = [CANVAS_WIDTH * 0.5, CANVAS_HEIGHT * 0.95];
     drawBranches(branchCanvas, treeLocation, 100, 0, 15);
+    drawLeaves(branchCanvas)
+}
+
+function drawLeaves(branchCanvas) {
+    const ctx = branchCanvas.getContext('2d');
+    const imageData = ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const data = imageData.data;
+
+    let branchPixels = []
+    for (let i = 0; i < CANVAS_HEIGHT; i++) {
+        for (let j = 0; j < CANVAS_WIDTH; j++) {
+            let red = data[4 * (i * CANVAS_WIDTH + j) + 0];
+            let green = data[4 * (i * CANVAS_WIDTH + j) + 1];
+            let blue = data[4 * (i * CANVAS_WIDTH + j) + 2];
+            let alpha = data[4 * (i * CANVAS_WIDTH + j) + 3];
+
+            if (alpha > 0 && i < CANVAS_HEIGHT * 0.95 - 100) {
+                branchPixels.push([j, i]);
+            }
+        }
+    }
+
+    for (let i = 0; i < branchPixels.length; i++) {
+        if (Math.random() < 0.3) {
+            ctx.beginPath();
+            ctx.fillStyle = 'rgba(0, 255, 0, 1)';
+            ctx.arc(...branchPixels[i], 10, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
 }
 
 function drawBranches(canvas, start, len, angle, branchWidth) {
